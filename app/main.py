16405a8ae08203d7fef25ed7f817f8445335fd8b
lala <cloudlala0@gmail.com>
2025-04-07 14:25:36 +0900

    all 영화추천 완성

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+import pandas as pd
 from fastapi import FastAPI
 import uvicorn
-from resolver import random_items, random_genres_items,random_genres_items_best
+from starlette.middleware.cors import CORSMiddleware
 from recommender import item_based_recommendation
-import pandas as pd
+from resolver import random_items, random_genres_items, random_genres_items_best
+
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
 @app.get("/")
 async def root():
     return {"message": "Hello MovieR"}
@@ -11,23 +26,22 @@ async def root():
 @app.get("/all")
 async def all_movies():
     result = random_items()
-    return{"result":result}
+    return {"result": result}
 
 @app.get("/genres/{genre}")
 async def genre_movies(genre: str):
     result = random_genres_items(genre)
-    return{"result": result}
+    return {"result": result}
 
 @app.get("/genresbest/{genre}")
-async def genre_movies(genre: str):
+async def genre_movies_best(genre: str):
     result = random_genres_items_best(genre)
-    return{"result": result}
+    return {"result": result}
 
 @app.get("/item_based/{item_id}")
 async def item_based(item_id: str):
     result = item_based_recommendation(item_id)
-    return{"result":result}
+    return {"result": result}
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-
---
+++ front/movie_js.js
@@ -0,0 +1,45 @@
+let MovieObject = {
+    init: function(){
+    },
+
+    getall: function(){
+       $.ajax({
+       // 실행할 코드
+          type: "GET",
+          url: "http://localhost:8000/all",
+       }).done(function(response){
+       // 성공코드
+            console.log(response)
+            movielist = response.result
+
+            topdiv = document.createElement("div")
+            topdiv.style = 'column-count:5'
+            document.body.appendChild(topdiv)
+
+           movielist.forEach(movie =>{
+               cmovie = document.createElement("div")
+               cmovie.className = "card"
+
+               mimg = document.createElement("img")
+               mimg.className = "card-img-top"
+               mimg.src = movie.poster_path
+               mimg.onclick = function(){
+                   // window.location.href = movie.url <-- 현재창 변경
+                   window.open(movie.url)
+               }
+
+               mimg.style.cursor = "pointer"
+
+               cmovie.appendChild(mimg)
+               topdiv.appendChild(cmovie)
+
+           });
+       }).fail(function(error){
+           // 실패코드
+           console.log(error)
+       });
+    }
+}
+
+MovieObject.init();
+MovieObject.getall();
